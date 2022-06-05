efe0581c2b37f4b646b4ce50c518d8217f3c1bc3
Elixir <96543010+Elixir-OP@users.noreply.github.com>
2022-06-05 18:55:34 +0500

    Update Manifest

---
+++ ex-scoreboard/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Elixir FW'
+description 'ex-scoreboard for QBCORE'
+version '1.2.0'
+
+client_scripts {
+   'cl_scoreboard.lua',
+   'warmenu.lua'
+}
+
+server_scripts {
+   'sv_scoreboard.lua'
+}
